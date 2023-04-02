fe21884ff905f5e525d97ae2f217558d0faad154
joneslee0918 <joneslee0918@gmail.com>
2023-04-02 14:14:13 +0300

    updated content

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $("select").niceSelect();
   $("#sex_option").next("div").find(".current").text("Seleccionar");
+  $("#sex_option").next("div").find(".current").css("color","#ccc");
 });
 
 var sex_option = 0;
@@ -10,10 +11,10 @@ var height_option = 0;
 var activity_option = 1;
 function calculateCalories(sex, age, weight, height, activityLevel) {
   // set constant values for calculations based on sex
-  const bmrConstant = sex === 1 ? 88.362 : 447.593;
-  const heightConstant = sex === 1 ? 4.799 : 3.098;
-  const weightConstant = sex === 1 ? 13.397 : 9.247;
-  const ageConstant = sex === 1 ? 5.677  : 4.330 
+  const bmrConstant = sex === "1" ? 66.5 : 655.1;
+  const heightConstant = sex === "1" ? 5.003 : 1.85;
+  const weightConstant = sex === "1" ? 13.75 : 9.563;
+  const ageConstant = sex === "1" ? 6.75  : 4.676
   // calculate basal metabolic rate (BMR)
   const bmr = bmrConstant + (weightConstant * weight) + (heightConstant * height) - (age * ageConstant);
   // assign activity level multiplier based on selection
@@ -42,6 +43,7 @@ function calculateCalories(sex, age, weight, height, activityLevel) {
 }
 
 $("#sex_option").change(function() {
+  $("#sex_option").next("div").find(".current").css("color","#4c84b7");
   sex_option = $(this).val();
 });
 
@@ -68,10 +70,10 @@ $("#calculate").click(function() {
 });
 
 $("#recalculate").click(function() {
-  $("#sex_option").val(0);
-  $("#age_option").val(0);
-  $("#weight_option").val(0);
-  $("#height_option").val(0);
+  $("#sex_option").val('');
+  $("#age_option").val('');
+  $("#weight_option").val('');
+  $("#height_option").val('');
   $("#check1").prop('checked',true);
   sex_option = 0;
   age_option = 0;
@@ -79,5 +81,7 @@ $("#recalculate").click(function() {
   height_option = 0;
   $("#query").css("display","block");
   $("#answer").css("display","none");
+  $("#sex_option").next("div").find(".current").text("Seleccionar");
+  $("#sex_option").next("div").find(".current").css("color","#ccc");
 });
 
